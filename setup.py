29fa6d64a20c6a0ddb812b4edc6ba3c4fcd2ce87
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2021-07-20 16:34:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_readme_content():
 
 setup(
     name = 'pywhistle',
-    version = '0.0.2',
+    version = '0.0.3',
     description = 'Unofficial Whistle 3 Device API',
     author = 'Chris F Ravenscroft',
     author_email = 'chris@voilaweb.com',
